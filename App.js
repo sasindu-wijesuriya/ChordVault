13fbc7c3a17543350923767f02117d980b143884
Sasindu Wijesuriya <sasindu@ackcio.com>
2024-08-02 17:25:22 +0530

    Refactor App.js to include SafeAreaView and SongsListScreen

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import SongsListScreen from "./screens/SongsListScreen";
+
+const currentUserName = "sasinduwije1";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SongsListScreen currentUserName={currentUserName} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
