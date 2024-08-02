1e4574283af470d93f79ea58de3a18ac4fb56614
Sasindu Wijesuriya <sasindu@ackcio.com>
2024-08-02 18:20:53 +0530

    Add authentication functionality to App.js

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import SongsListScreen from "./screens/SongsListScreen";
+import { useState } from "react";
 
-const currentUserName = "sasinduwije1";
+import SongsListScreen from "./screens/SongsListScreen";
+import AuthenticationByEmailScreen from "./screens/AuthenticationByEmailScreen";
+
+// const currentUserName = "sasinduwije1";
+let screen;
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userName, setUserName] = useState("Default");
+
+  function handleAuthentication(isLoggedIn, userName = "Default") {
+    if (isLoggedIn) {
+      setUserName(userName);
+      setIsAuthenticated(true);
+      console.log("Authentication : Logged in as", userName);
+    } else {
+      setIsAuthenticated(false);
+      setUserName("Default");
+      console.log("Authentication : Logged out");
+    }
+  }
+
+  function logoutHandler() {
+    handleAuthentication(false);
+    console.log("Logging out...");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <SongsListScreen currentUserName={currentUserName} />
+      <View>
+        {isAuthenticated ? (
+          <SongsListScreen
+            currentUserName={userName}
+            onLogoutPress={logoutHandler}
+          />
+        ) : (
+          <AuthenticationByEmailScreen
+            onAuthenticationDone={handleAuthentication}
+          />
+        )}
+      </View>
     </SafeAreaView>
   );
 }
@@ -15,5 +50,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
+    flex: 1,
+    padding: 20,
+  },
+  loggedOutContainer: {
+    backgroundColor: "#b92c2c",
+  },
+  loggedInContainer: {
+    backgroundColor: "#15dd5e",
   },
 });
