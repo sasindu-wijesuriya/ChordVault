a9a1fe9afdb6a752e57780a511d5fe5720103d14
Sasindu Wijesuriya <sasindu@ackcio.com>
2024-08-02 18:20:46 +0530

    Add AuthenticationByEmailScreen component

---
+++ screens/AuthenticationByEmailScreen.js
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "@firebase/auth";
+import { app } from "../firebase";
+
+const AuthScreen = ({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  isLogin,
+  setIsLogin,
+  handleAuthentication,
+}) => {
+  return (
+    <View style={styles.authContainer}>
+      <Text style={styles.title}>{isLogin ? "Sign In" : "Sign Up"}</Text>
+
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Email"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title={isLogin ? "Sign In" : "Sign Up"}
+          onPress={handleAuthentication}
+          color="#3498db"
+        />
+      </View>
+
+      <View style={styles.bottomContainer}>
+        <Text style={styles.toggleText} onPress={() => setIsLogin(!isLogin)}>
+          {isLogin
+            ? "Need an account? Sign Up"
+            : "Already have an account? Sign In"}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+AuthenticationByEmailScreen = ({ onAuthenticationDone }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isUserAuthorized, setUser] = useState(null); // Track user authentication state
+  const [isLogin, setIsLogin] = useState(true);
+
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  const handleAuthentication = async () => {
+    try {
+      if (isUserAuthorized) {
+        // If user is already authenticated, log out
+        onAuthenticationDone(false);
+        await signOut(auth);
+        console.log("User logged out successfully!");
+      } else {
+        // Sign in or sign up
+        if (isLogin) {
+          // Sign in
+          await signInWithEmailAndPassword(auth, email, password);
+          console.log("User signed in successfully!");
+          onAuthenticationDone(true, email.split("@")[0]);
+        } else {
+          // Sign up
+          await createUserWithEmailAndPassword(auth, email, password);
+          console.log("User created successfully!");
+          onAuthenticationDone(true, email.split("@")[0]);
+        }
+      }
+    } catch (error) {
+      console.error("Authentication error:", error.message);
+    }
+  };
+
+  return (
+    // Show sign-in or sign-up form if user is not authenticated
+    <AuthScreen
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      isLogin={isLogin}
+      setIsLogin={setIsLogin}
+      handleAuthentication={handleAuthentication}
+    />
+  );
+};
+
+export default AuthenticationByEmailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    backgroundColor: "#f0f0f0",
+  },
+  authContainer: {
+    width: "80%",
+    maxWidth: 400,
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    borderRadius: 4,
+  },
+  buttonContainer: {
+    marginBottom: 16,
+  },
+  toggleText: {
+    color: "#3498db",
+    textAlign: "center",
+  },
+  bottomContainer: {
+    marginTop: 20,
+  },
+  emailText: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
